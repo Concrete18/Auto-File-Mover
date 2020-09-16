f06bd214c1fc48fac0872934c798ad248336af41
Concrete18 <michaelericson19@gmail.com>
2020-09-16 19:22:41 -0400

    small changes

--- test_Auto_Folder_Cleaner.py
+++ test_Auto_Folder_Cleaner.py
@@ -9,8 +9,7 @@ class TestAFC(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        print('Setting Up Test')
-        files = ['videofile.mkv', 'wallpaper.png', 'python.mp4', 'python.py', 'wallpaper.png.zip']
+        files = ['videofile.mkv', 'wallpaper.png', 'python.mp4', 'python.py', 'wallpaper.png.zip', 'installer.exe']
         cls.main_dir = os.getcwd()
         cls.test_dir = os.path.join(cls.main_dir, 'testing')
         if not os.path.exists(cls.test_dir):
@@ -23,13 +22,11 @@ class TestAFC(unittest.TestCase):
         cls.files = {}
         for _file in os.scandir(cls.test_dir):
             cls.files[_file.name] = _file
-        print(cls.files)
         os.chdir(cls.main_dir)
 
 
     @classmethod
     def tearDownClass(cls):
-        print('Ending Test')
         for _file in os.scandir(cls.test_dir):
             if os.path.exists(_file.path) and not os.path.isfile(_file.path):
                 os.rmdir(_file.path)
@@ -37,10 +34,10 @@ class TestAFC(unittest.TestCase):
                 os.remove(_file.path)
         os.rmdir(cls.test_dir)
 
-
+    # FIXME File_Move function.
     # def test_File_Move(self):
     #     hd_video_dir = os.path.join(self.test_dir, 'HD Video')
-    #     File_Move(self.files[1], hd_video_dir)
+    #     File_Move(self.files['videofile.mkv'], hd_video_dir)
     #     self.assertTrue(os.path.exist(os.path.join(self.test_dir, 'HD Video', 'videofile.mkv')))
 
 
@@ -57,9 +54,10 @@ class TestAFC(unittest.TestCase):
         self.assertEqual(Get_File_Type(self.files['wallpaper.png']), ".png")
         self.assertEqual(Get_File_Type(self.files['wallpaper.png.zip']), ".zip")
 
+    # FIXME Mock input in Delete_Check function.
     # def test_Delete_Check(self):
     #     # Tests to see if function asks to delete files in delete_def.
-    #     self.assertEqual(Set_Destination('C:/Downloads', 'installer.exe'), "skip")
+    #     self.assertEqual(Set_Destination('C:/Downloads', self.files['installer.exe']), "skip")
 
 
 if __name__ == '__main__':
